2954f8cc60696cbf49331eb9eb31d9e550b56af1
NacevskiD <nacevskidavid@gmail.com>
2017-02-01 00:15:00 -0600

    Update CreditCard.java

--- src/main/java/com/clara/CreditCard.java
+++ src/main/java/com/clara/CreditCard.java
@@ -27,8 +27,50 @@ public class CreditCard {
 
     public static boolean isValidCreditCard(String cc) {
 
-        //TODO Replace with your code to process the credit card number, and determine if it is valid.
-        //TODO Make sure all the tests pass!
+       int acumulator = 0;
+        int[] doubleDigits = new int[8];
+        int[] creditCard = new int[cc.length()];
+        int[] seperateDigits = new int[8];
+        // checking if there are 16 numbers
+        if (cc.length() != 16) {
+            return false;
+        }
+        for (int count = 0;count < cc.length();count++) {
+            // converting the string to an individual char, then string, then int and adding it to the credit card array
+            int number = Integer.parseInt(Character.toString(cc.charAt(count)));
+            creditCard[count] = number;
+        }
+        // checking if the credit card starts with 4
+        if (creditCard[0] != 4){
+            return false;
+        }
+        int ddCounter = 0;
+        //multiplying every second number by 2 and adding it to the doubleDigits array
+        for (int count = 0; count < cc.length();count+=2){
+            doubleDigits[ddCounter] = creditCard[count] * 2;
+            ddCounter++;
+        }
+        ddCounter = 0;
+        //adding all the individual number to the acumulator
+        for (int count = 1; count < cc.length();count+=2){
+            // checking if there is a remainder, if there is adding both numbers, if there isn't adding only the first number
+            int digit1 = doubleDigits[ddCounter] / 10;
+            int digit2 = doubleDigits[ddCounter] % 10;
+            if (digit1 == 0){
+                acumulator+=doubleDigits[ddCounter];
+            }
+            else {
+                acumulator+= digit1 + digit2;
+            }
+            ddCounter++;
+            acumulator+= creditCard[count];
+        }
+        //checking if there is a remainder in the acumulator, if there is it fails , if there isn't it passes.
+        int check = acumulator % 10;
+        if (check != 0){
+            return false;
+        }
+
 
         return false;
 
